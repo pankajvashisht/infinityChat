489722e3ab597bc2caf1133d7c8ca89162dccdc2
pankajvashisht-ucreate <iampankajvashisht@gmail.com>
2023-04-03 17:21:34 +0530

    fixed the issue

--- server/src/Controller/v1/UserController.js
+++ server/src/Controller/v1/UserController.js
@@ -1,8 +1,8 @@
-const ApiController = require('./ApiController');
-const app = require('../../../libary/CommanMethod');
-const Db = require('../../../libary/sqlBulider');
-const ApiError = require('../../Exceptions/ApiError');
-const { lang } = require('../../../config');
+const ApiController = require("./ApiController");
+const app = require("../../../libary/CommanMethod");
+const Db = require("../../../libary/sqlBulider");
+const ApiError = require("../../Exceptions/ApiError");
+const { lang } = require("../../../config");
 const DB = new Db();
 
 class UserController extends ApiController {
@@ -24,14 +24,14 @@ class UserController extends ApiController {
 				Request.files.document
 			);
 		}
-		const userID = await DB.save('users', RequestData);
+		const userID = await DB.save("users", RequestData);
 		RequestData.lang = Request.lang;
 		setTimeout(() => {
 			this.mails(RequestData);
 		}, 100);
 		const userInfo = await super.userDetails(userID);
 		if (userInfo.profile.length > 0) {
-			userInfo.profile = appURL + 'uploads/' + userInfo.profile;
+			userInfo.profile = appURL + "uploads/" + userInfo.profile;
 		}
 		return {
 			message: lang[Request.lang].signup,
@@ -48,10 +48,10 @@ class UserController extends ApiController {
 			throw new ApiError(lang[req.lang].invaildOtp);
 		}
 		req.body.userInfo.status = 1;
-		await DB.save('users', req.body.userInfo);
+		await DB.save("users", req.body.userInfo);
 		const usersInfo = await super.userDetails(req.body.userInfo.id);
 		if (usersInfo.profile.length > 0) {
-			usersInfo.profile = appURL + 'uploads/' + usersInfo.profile;
+			usersInfo.profile = appURL + "uploads/" + usersInfo.profile;
 		}
 		return {
 			message: lang[req.lang].verifyOtp,
@@ -83,25 +83,25 @@ class UserController extends ApiController {
 		};
 
 		let request_data = await super.vaildation(required, non_required);
-		let soical_id = await DB.find('users', 'first', {
+		let soical_id = await DB.find("users", "first", {
 			conditions: {
 				or: {
 					email: request_data.email,
 					social_id: request_data.social_id,
 				},
 			},
-			fields: ['id'],
+			fields: ["id"],
 		});
 		if (soical_id) {
 			request_data.id = soical_id.id;
 		}
-		const id = await DB.save('users', request_data);
+		const id = await DB.save("users", request_data);
 		const userInfo = await super.userDetails(id);
 		if (userInfo.profile.length > 0) {
-			userInfo.profile = appURL + 'uploads/' + userInfo.profile;
+			userInfo.profile = appURL + "uploads/" + userInfo.profile;
 		}
 		return {
-			message: 'User login successfully',
+			message: "User login successfully",
 			data: userInfo,
 		};
 	}
@@ -113,24 +113,24 @@ class UserController extends ApiController {
 		};
 		let non_required = {};
 		let request_data = await super.vaildation(required, non_required);
-		let user_info = await DB.find('users', 'first', {
+		let user_info = await DB.find("users", "first", {
 			conditions: {
 				email: request_data.email,
 			},
-			fields: ['id', 'email', 'name'],
+			fields: ["id", "email", "name"],
 		});
 		if (!user_info) throw new ApiError(lang[req.lang].mailNotFound);
 		user_info.otp = request_data.otp;
 		user_info.forgot_password_hash = app.createToken();
-		await DB.save('users', user_info);
+		await DB.save("users", user_info);
 		let mail = {
 			to: request_data.email,
-			subject: 'Forgot Password',
-			template: 'forgot_password',
+			subject: "Forgot Password",
+			template: "forgot_password",
 			data: {
 				first_name: user_info.name,
 				last_name: user_info.name,
-				url: appURL + 'users/change_password/' + user_info.forgot_password_hash,
+				url: appURL + "users/change_password/" + user_info.forgot_password_hash,
 			},
 		};
 		setTimeout(() => {
@@ -149,28 +149,28 @@ class UserController extends ApiController {
 		};
 		const non_required = {
 			device_type: req.body.device_type || 0,
-			device_token: req.body.device_token || '',
+			device_token: req.body.device_token || "",
 			last_login: app.currentTime,
 			authorization_key: app.createToken(),
 		};
 
 		let request_data = await super.vaildation(required, non_required);
-		let login_details = await DB.find('users', 'first', {
+		let login_details = await DB.find("users", "first", {
 			conditions: {
 				email: request_data.email,
 			},
-			fields: ['id', 'password', 'status', 'email'],
+			fields: ["id", "password", "status", "email"],
 		});
 		if (login_details) {
 			if (request_data.password !== login_details.password)
 				throw new ApiError(lang[req.lang].wrongLogin);
 			delete login_details.password;
 			request_data.id = login_details.id;
-			await DB.save('users', request_data);
+			await DB.save("users", request_data);
 			login_details.authorization_key = request_data.authorization_key;
 			login_details = await super.userDetails(login_details.id);
 			if (login_details.profile.length > 0) {
-				login_details.profile = appURL + 'uploads/' + login_details.profile;
+				login_details.profile = appURL + "uploads/" + login_details.profile;
 			}
 			return {
 				message: lang[req.lang].LoginMessage,
@@ -181,9 +181,9 @@ class UserController extends ApiController {
 	}
 
 	async appInfo() {
-		const app_info = await DB.find('app_informations', 'all');
+		const app_info = await DB.find("app_informations", "all");
 		return {
-			message: 'App Informations',
+			message: "App Informations",
 			data: app_info,
 		};
 	}
@@ -198,9 +198,9 @@ class UserController extends ApiController {
 			throw new ApiError(lang[req.lang].oldPassword);
 		}
 		loginInfo.password = request_data.new_password;
-		await DB.save('users', loginInfo);
+		await DB.save("users", loginInfo);
 		return {
-			message: 'Password change Successfully',
+			message: "Password change Successfully",
 			data: [],
 		};
 	}
@@ -212,7 +212,7 @@ class UserController extends ApiController {
 			name: req.body.name,
 			country: req.body.country,
 			email: req.body.email,
-			phone: req.body.email,
+			phone: req.body.phone,
 		};
 		const request_data = await super.vaildation(required, non_required);
 		if (request_data.email) {
@@ -241,13 +241,13 @@ class UserController extends ApiController {
 		if (req.files && req.files.profile) {
 			request_data.profile = await app.upload_pic_with_await(req.files.profile);
 		}
-		await DB.save('users', request_data);
+		await DB.save("users", request_data);
 		const usersinfo = await super.userDetails(request_data.id);
 		if (usersinfo.profile.length > 0) {
-			usersinfo.profile = appURL + 'uploads/' + usersinfo.profile;
+			usersinfo.profile = appURL + "uploads/" + usersinfo.profile;
 		}
 		return {
-			message: 'Profile updated successfully',
+			message: "Profile updated successfully",
 			data: usersinfo,
 		};
 	}
@@ -257,23 +257,23 @@ class UserController extends ApiController {
 			id: req.body.user_id,
 		};
 		let request_data = await super.vaildation(required, {});
-		request_data.authorization_key = '';
-		request_data.device_token = '';
-		await DB.save('users', request_data);
+		request_data.authorization_key = "";
+		request_data.device_token = "";
+		await DB.save("users", request_data);
 		return {
-			message: 'User Logout successfully',
+			message: "User Logout successfully",
 			data: [],
 		};
 	}
 	mails(request_data) {
 		let mail = {
 			to: request_data.email,
-			subject: 'User Account Verification',
-			template: 'user_signup',
+			subject: "User Account Verification",
+			template: "user_signup",
 			data: {
 				first_name: request_data.name,
 				last_name: request_data.name,
-				url: appURL + 'users/verify/' + request_data.authorization_key,
+				url: appURL + "users/verify/" + request_data.authorization_key,
 			},
 		};
 		try {
